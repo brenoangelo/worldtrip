14947e2c93f24c1c5d8bb9a5b14d1315f7c0522c
Breno Angelo <brenoangeloal@gmail.com>
2022-05-31 22:13:15 -0300

    Implement banner header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Flex, Image } from '@chakra-ui/react';
 export function Header() {
   return (
     <Flex as="header" justify="center" align="center" h={100}>
-      <Image src="/assets/icons/logo.svg" alt="" h={45.9} />
+      <Image src="/assets/icons/logo.svg" alt="" w="46" />
     </Flex>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,32 @@
+import { Box, Container, Flex, Heading, Image, Stack, Text } from '@chakra-ui/react';
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <Flex
+      bgImg="/assets/images/background.svg"
+      bgRepeat="no-repeat"
+      bgSize="100%"
+      bgPosition="center"
+      minH={335}
+    >
+      <Container maxW={1160} h="100%" maxH={335}>
+        <Flex justify="space-between" align="center">
+          <Stack color="gray.100">
+            <Heading>
+              5 Continentes, <br />
+              infinitas possibilidades.
+            </Heading>
+
+            <Text>
+              Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+            </Text>
+          </Stack>
+
+          <Image
+            src="/assets/icons/airplane.svg"
+          />
+        </Flex>
+      </Container>
+    </Flex>
+  );
 }
