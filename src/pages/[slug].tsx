d94c0f4b59e04cd6ed0b27b6c27c5a89e9b70b95
Breno Angelo <brenoangeloal@gmail.com>
2022-06-08 22:09:16 -0300

    Start a continent page implementation

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,5 +1,51 @@
-export default function ContinentPage() {
+import { Flex, Heading, Image } from '@chakra-ui/react';
+import { GetServerSideProps } from 'next';
+import { useEffect } from 'react';
+import { api } from '../services/api';
+
+type ContinentPage = {
+  slug: string;
+};
+
+export default function ContinentPage({ slug }: ContinentPage) {
+  useEffect(() => {
+    api
+      .get(`continents/${slug}`)
+      .then((response) => console.log(response.data));
+  }, []);
+
   return (
-    <h2>teste</h2>
-  )
+    <Flex direction="column">
+      <Flex position="relative">
+        <Heading
+          as="h1"
+          position="absolute"
+          bottom={59}
+          left={140}
+          color="gray.100"
+          fontSize="5xl"
+          fontWeight={600}
+        >
+          Europa
+        </Heading>
+        <Image
+          src="/assets/images/europa.jpg"
+          w="100%"
+          align="center"
+          objectFit="cover"
+          maxH={500}
+        />
+      </Flex>
+    </Flex>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { slug } = params;
+
+  return {
+    props: {
+      slug,
+    },
+  };
+};
