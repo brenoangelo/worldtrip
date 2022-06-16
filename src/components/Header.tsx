9fd63fc28d3161d87522e836bde9ba2fe3cdb07e
Breno Angelo <brenoangeloal@gmail.com>
2022-06-16 19:48:39 -0300

    create link in logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
-import { Flex, Image } from '@chakra-ui/react';
+import { Flex, Image, Link } from '@chakra-ui/react';
 
 export function Header() {
   return (
     <Flex as="header" justify="center" align="center" h={100}>
-      <Image src="/assets/icons/logo.svg" alt="" w="46" />
+      <Link href="/">
+        <Image src="/assets/icons/logo.svg" alt="" w="46" />
+      </Link>
     </Flex>
   );
 }
